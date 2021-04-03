3d81e63ee5d1a7381c660c3efd2d1a7565a34068
Brienne <cole.brienne@gmail.com>
2021-04-03 15:50:01 -0700

    stuck but committing anyway

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,24 @@
 // from data.js
-let tableData = data;
+var tableData = data;
 const tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
-// View Data
-console.log(tableData)
+// View table
+//console.log(tableData)
+
+// Create function to import data into table
+function generateTable(tableData) {
+    tableData.forEach(sighting => {
+        let row = tbody.append("tr");
+        Object.values(sightings).forEach()
+    })
+}
+
+//Input table
+generateTable(tableData);
+
+//Create Filter Button
+//Event listener
+var button = d3.select("#filter-btn"); {
+    button.on
+}
