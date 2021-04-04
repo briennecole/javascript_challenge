4fc20a0d8f9e179d5521914903ad0c1c7db7fc8d
Brienne <cole.brienne@gmail.com>
2021-04-03 21:53:53 -0700

    no wins to report yet

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,14 +8,14 @@ const tbody = d3.select("tbody");
 
 // Create function to import data into table
 function generateTable(tableData) {
-    tableData.forEach(sightings => {
+    tableData.forEach((sightings) => {
         let row = tbody.append("tr");
-        Object.values(sightings).forEach(value => {
-            let object = row.append("td");
-            object.text(value);
+        Object.entries(sightings).forEach(([key, value]) => {
+            let cell = row.append("td");
+            cell.text(value);
         });
     });
-}
+};
 
 //Input table
 generateTable(tableData);
@@ -24,7 +24,10 @@ generateTable(tableData);
 var button = d3.select("#filter-btn");
 
 //Create event listener 
-button.on("click", runEnter);
+button.on("click", function() {
+    console.log("Hi, a button was clicked");
+
+// button.on("click", runEnter);
 
 function runEnter() {
 
@@ -42,16 +45,16 @@ function runEnter() {
     //console.log(inputValue);
 
     //Show filtered data in table for selected date
-    //var filterDate = tableData.filter(sightings => sightings.datetime === inputValue);
-    //console.log(filterDate)
+    var filterDate = tableData.filter(sightings => sightings.datetime === inputValue);
+    console.log(filterDate)
 
     // clears the html page before displaying the filtered rows
     tbody.html("")
     // Use d3 to append one table row `tr` for each ufo data object
-    filterDate.forEach(sightings => {
-        let row = tbody.append("tr");
-        Object.values(sightings).forEach(value => {
-            let object = row.append("td");
+    filterDate.forEach((sightings) => {
+        var row = tbody.append("tr");
+        Object.entries(sightings).forEach(([key, value]) => {
+            var object = row.append("td");
             object.text(value);
         });
     });
